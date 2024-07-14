7eede968b9451e9e47784218ce36db07719f9c5b
salarhseinz <salarhseinz123@gmail.com>
2024-07-15 03:03:09 +0330

    Example 2 mainactivity project

--- app/src/main/java/com/example/androidfanavari40205/MainActivity.kt
+++ app/src/main/java/com/example/androidfanavari40205/MainActivity.kt
@@ -5,16 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidfanavari40205.databinding.ActivityMainBinding
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //1
         setContentView(R.layout.activity_main)
+        val buttonTodo : MaterialButton = findViewById(R.id.buttonTodoList)
+    //روش غیراستاندارد برای صدا کردن 1
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonEditProfile.id
+
+        /*enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-    }
-}
+        }*/
+    }}
